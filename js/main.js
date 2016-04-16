2c3e78aad2d901b067537dec3bb622a2820704f8
Lachlan Burndred <toomenvy@gmail.com>
2016-04-16 22:50:38 +1000

    Initial Commit
    Adding files to GitHub. First Commit.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+context.beginPath();
+context.arc(50, 50, 50, 0, Math.PI * 2);
+context.fill();
+context.closePath();
