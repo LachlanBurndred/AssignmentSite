16de815de349f406c7f917fa8ce386bb567218cf
Lachlan Burndred <toomenvy@gmail.com>
2016-04-16 23:17:54 +1000

    Asset script creation
    Added handling of deltaTime.

---
+++ js/assets.js
@@ -0,0 +1,26 @@
+﻿var FPS = 60.0;
+
+var Time = {
+    now: 0,
+    lastUpdate: 0,
+    deltaTime: 0,
+    Update: function()
+    {
+        this.now = Date.now();
+        this.deltaTime = (this.now - this.lastUpdate) / 1000;
+        this.lastUpdate = Date.now();
+    }
+} // Handles deltaTime
+
+// Main update function
+function Update()
+{
+    
+}
+
+// Sets to run update every frame @ 60fps
+setInterval(function ()
+{
+    Time.Update();
+    Update();
+}, 1000 / FPS);
--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 var context = canvas.getContext("2d");
 
 context.beginPath();
-context.arc(50, 50, 50, 0, Math.PI * 2);
+context.arc(50, 50, 30, 0, Math.PI * 2);
 context.fill();
 context.closePath();
